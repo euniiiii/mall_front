23d819b4a87120f27fe8cee20ad70f7ae69f51d0
euniiiii <hee9920@naver.com>
2024-03-27 12:50:57 +0900

    todo

---
+++ src/pages/todo/ListPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ListComponent from '../../components/todo/ListComponent';
+
+function ListPage() {
+    return (
+        <div className='p-4 w-full bg-white'>
+            <div className='text-3xl font-extrabold'>
+                Todo List Page component
+            </div>
+            <ListComponent/>
+        </div>
+    );
+}
+
+export default ListPage;
---
+++ src/pages/todo/ModifyPage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import ModifyComponent from '../../components/todo/ModifyComponent';
+
+const ModifyPage = () => {
+
+    const {tno} = useParams()
+
+    const navigate = useNavigate()
+
+    const moveToRead = () => {
+        navigate({pathname: `/todo/read/${tno}`})
+    }
+
+    const moveToList = () => {
+        navigate({pathname: `/todo/list`})
+    }
+
+    return (
+        <div className="p-4 w-full bg-white">
+            <div className="text-3xl font-extrabold">
+            Todo Modify Page  
+            </div> 
+
+            <ModifyComponent tno={tno}/>
+        </div>
+    );
+}
+
+export default ModifyPage;
